150be2736171037c4ba8b9236f9559908c3b0ca5
JHenrique <henryque.pc.game@gmail.com>
2025-07-29 16:00:47 -0300

    Proteger Rotas da API

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,9 @@ use App\Services\ApiResponse;
 
 Route::get('/status', function() {
     return ApiResponse::success('API is running');
-});
+})->middleware('auth:sanctum');
 
-Route::apiResource('/clients', ClientController::class);
+Route::apiResource('/clients', ClientController::class)->middleware('auth:sanctum');
 
 // auth routes
 Route::post('/login', [AuthController::class, 'login']);
